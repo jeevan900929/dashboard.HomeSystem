26dcb0837b437ab7dac0832ac07da30551634773
jeevanAnga <jeevan.900929@gmail.com>
2017-10-26 18:59:07 +0800

    Added label

--- dashboard.HomeSystem/Form1.vb
+++ dashboard.HomeSystem/Form1.vb
@@ -1,3 +1,7 @@
 ﻿Public Class Form1
-
+    Dim Count
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+        Label1.Text = Count
+        Count = Count + 1
+    End Sub
 End Class
